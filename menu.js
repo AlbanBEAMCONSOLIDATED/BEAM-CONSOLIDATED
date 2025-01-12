27d8154ce19e4be930740b8f2393e12b3d0bfe4c
AlbanBEAMCONSOLIDATED <alban.bicj@gmail.com>
2025-01-12 17:09:34 +0100

    Delete menu.js

--- menu.js
+++
@@ -1,9 +0,0 @@
-const mobileMenuButton2 = document.getElementById('mobile-menu-button');
-const navLinks2 = document.getElementById('nav-links');
-
-if (mobileMenuButton2 && navLinks2) {
-  mobileMenuButton2.addEventListener('click', () => {
-    navLinks2.classList.toggle('active');
-    mobileMenuButton2.classList.toggle('open');
-  });
-}
